713d0d4eaef6f92fe5d659e4037421adfab2eaf5
Broström.A | Evul <andreas.brostrom.ce@gmail.com>
2016-04-01 16:01:43 +0200

    Delete script_component.hpp

--- Addons/propaganda/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#define COMPONENT propaganda
-#include "\z\TOBlackOrder\addons\main\script_mod.hpp"
-
-#include "\z\TOBlackOrder\addons\main\defines.hpp"
-#include "\z\TOBlackOrder\addons\main\script_macros.hpp"
