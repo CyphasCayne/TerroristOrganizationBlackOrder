1da0880deb5327da89a4f962ac134d7f155bf225
ColdEvul <andreas.brostrom.ce@gmail.com>
2016-04-05 19:51:46 +0200

    Added Vehicles

---
+++ Addons/vehicle_car/config.cpp
@@ -0,0 +1,25 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {
+            "TERROR_VEH_O_m1025",
+            "TERROR_VEH_O_m1025_m2",
+            "TERROR_VEH_O_m1025_Mk19",
+            "TERROR_VEH_O_SUV"
+        };
+        weapons[] = {
+        };
+        magazines[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "TOBlackOrder_Main",
+            "rhsusf_hmmwv"
+        };
+        VERSION_CONFIG;
+        author[] = {"Brostrom.A (Evul)"};
+        authorUrl = "http://dev.evul.nu/";
+	};
+};
+
+#include "CfgVehicles.hpp"
